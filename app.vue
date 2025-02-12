4f29f9d16798f3e16088edb64cf1a863b5bac1b0
Yuta Inoue <58018461+YutaInouePH@users.noreply.github.com>
2025-02-12 14:51:50 +0900

    add connection page and display messages

--- app.vue
+++ app.vue
@@ -1,6 +1,35 @@
+<script lang="ts" setup>
+const echo = useEcho()
+
+const messages = ref<string[]>([])
+const writeNewMessage = (e: object) => messages.value.push(JSON.stringify(e.message))
+
+function stopAllListeners() {
+  echo.leaveAllChannels()
+}
+
+function subscribeToPublicChannel() {
+  const name = 'welcome'
+  const event = '.WelcomeNotification'
+
+  echo
+    .channel(name)
+    .listen(event, (e: object) => {
+      writeNewMessage(e)
+    })
+    .error((e: object) => {
+      console.error('Public channel error', e)
+    })
+}
+
+onMounted(() => {
+  subscribeToPublicChannel();
+})
+</script>
+
 <template>
+  <h1>Reverb conncetion test page</h1>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <p v-for="message in messages" :key="message">{{ message }}</p>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['nuxt-laravel-echo'],
+
+  echo: {
+    key: '',
+    broadcaster: 'reverb', // available: reverb, pusher
+    host: '',
+    port: ,
+    scheme: '', // available: http, https
+    transports: ['ws', 'wss'],
+    authentication: {
+      mode: 'cookie',
+      baseUrl: 'http://localhost:80',
+      authEndpoint: '/broadcasting/auth',
+      csrfEndpoint: '/sanctum/csrf-cookie',
+      csrfCookie: 'XSRF-TOKEN',
+      csrfHeader: 'X-XSRF-TOKEN',
+    },
+    logLevel: 3,
+    properties: undefined,
+  },
+
+  vite: {
+    optimizeDeps: {
+      include: ['pusher-js'],
+    },
+  },
 })
